7ceaeabfd971a3fab14e40d17b4a32efb66d15ea
Kira Roubin <kirao@duck.com>
2023-03-16 16:31:51 +0300

    fix: update channel

--- app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
+++ app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
@@ -9560,7 +9560,7 @@ public class Tdlib implements TdlibProvider, Settings.SettingsChangeListener {
         int buildNo = 0;
         String version = null;
         String commit = null;
-        final String prefix = "Telegram-X-";
+        final String prefix = "Moegram-X-";
         if (!StringUtils.isEmpty(document.fileName) && document.fileName.startsWith(prefix)) {
           int i = document.fileName.indexOf('-', prefix.length());
           version = document.fileName.substring(prefix.length(), i == -1 ? document.fileName.length() : i);
