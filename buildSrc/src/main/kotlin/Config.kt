d9f0d39bb3aa61b5944cad2a7c74e359ccc226b3
Kira Roubin <kirao@duck.com>
2023-03-16 16:31:51 +0300

    fix: update channel

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -81,7 +81,7 @@ object Telegram {
   const val RESOURCES_CHANNEL = "Y2xvdWRfdGd4X2FuZHJvaWRfcmVzb3Vy"
 
   // Channel where to look up for new apks
-  const val UPDATES_CHANNEL = "tgx_log"
+  const val UPDATES_CHANNEL = "moex_log"
 
   // Language pack on server. Do not change
   const val LANGUAGE_PACK = "android_x"
