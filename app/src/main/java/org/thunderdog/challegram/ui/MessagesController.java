10acad4fa9e0657813a590b9c12d9951b88ea37c
Kira Roubin <kirao@duck.com>
2023-06-08 14:42:44 +0300

    add: repeat message

--- app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
@@ -5399,6 +5399,9 @@ public class MessagesController extends ViewController<MessagesController.Argume
             runOnUiThreadOptional(() -> SystemUtils.copyFileToClipboard(file, R.string.CopiedSticker));
           }
         });
+      } else if (id == R.id.btn_msgRepeat) {
+        TdApi.Message msg = selectedMessage.getMessage();
+        tdlib.client().send(new TdApi.ForwardMessages(msg.chatId, msg.messageThreadId, msg.chatId, new long[] {msg.id}, null, true, false, false), tdlib.messageHandler());
       } else if (id == R.id.btn_openIn) {
         if (selectedMessageTag != null) {
           TdApi.Document document = ((TdApi.MessageDocument) selectedMessage.getMessage().content).document;
